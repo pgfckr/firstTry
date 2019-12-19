ad83c309b0fa634296d51f6048cc4fccf120f324
pgfckr <smuelle1@stud.hs-heilbronn.de>
2019-12-19 04:47:14 +0100

    main now loops

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print ("Hello World.")
-x = input("number1: ")
-y = input("number2: ")
-
-print (float(x) + float(y))
-input ("press any key to continue")
+run = True
+while run:
+  x = input("number1: ")
+  y = input("number2: ")
+  print (float(x) + float(y))
+  if(input ("exit to exit")== "exit"): 
+    run = False
