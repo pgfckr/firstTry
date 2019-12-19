4dd71eec09e869408b7629c69d9500d2b5118706
shentox <r.iwan@live.de>
2019-12-19 05:26:54 +0100

    array

---
+++ array.py
@@ -0,0 +1,12 @@
+weed = ["white widow", "orange bud", "bubble kush"]
+print(weed)
+weed1 = "white widow"
+weed2 = "orange bud"
+weed3 = "bubble kush"
+
+x = weed[0]
+
+print(x)
+
+for x in weed:
+    print(x)
